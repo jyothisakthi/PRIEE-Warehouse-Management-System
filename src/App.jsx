5c4f1b57df770e60e9e7d3cd5dcf5401fce1a8ca
jyothi sakthi <220701113@rajalakshmi.edu.in>
2025-02-28 18:46:45 +0530

    added login and signup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+
+import LoginSignup from './components/loginSignup'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LoginSignup/>
+      
+    </>
+  )
+}
+
+export default App
