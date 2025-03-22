2946258068a8585c828bb0e91249d3c728e106f5
jyothi sakthi <220701113@rajalakshmi.edu.in>
2025-03-22 18:36:15 +0530

    sep for nav and sidebar

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import { FaHome, FaBox, FaShoppingCart, FaMoneyBillWave, FaChartBar, FaBell, FaS
 import { MdExpandMore, MdExpandLess } from "react-icons/md"; 
 import "./home.css";
 import TopSellingProducts from "./components/TopSellingProducts";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
 
 const Home = () => {
@@ -35,71 +37,12 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            {/* Navbar */}
-            <div className="navbar">
-                <div className="logo">WMS</div>
-                <div className="search-container">
-                    <FaSearch className="search-icon" />
-                    <input type="text" placeholder="Search..." className="search-bar" />
-                </div>
-                <div className="nav-icons">
-                    <button className="icon-button"><FaBell /></button>
-                    <button className="login-btn" onClick={() => navigate("/login")}>Login</button>
-                    <button className="signup-btn" onClick={() => navigate("/signup")}>Signup</button>
-                </div>
-            </div>
+            <Navbar />
 
             {/* Main Layout */}
             <div className="main-container">
                 {/* Left Pane */}
-                <div className="left-pane">
-                    <ul>
-                        <li><FaHome className="menu-icon" /> Home</li>
-
-                        {/* Inventory Section */}
-                        <li onClick={() => toggleMenu("inventory")}> 
-                            <FaBox className="menu-icon" /> Inventory {openMenu.inventory ? <MdExpandLess /> : <MdExpandMore />}
-                        </li>
-                        {openMenu.inventory && (
-                            <ul className="submenu">
-                                <li>   Items</li>
-                                <li>   Item Group</li>
-                            </ul>
-                        )}
-
-                        {/* Sales Section */}
-                        <li onClick={() => toggleMenu("sales")}> 
-                            <FaShoppingCart className="menu-icon" /> Sales {openMenu.sales ? <MdExpandLess /> : <MdExpandMore />}
-                        </li>
-                        {openMenu.sales && (
-                            <ul className="submenu">
-                                <li>   Customers</li>
-                                <li>   Sales Orders</li>
-                                <li>   Packages</li>
-                                <li>   Shipments</li>
-                                <li>   Invoices</li>
-                                <li>   Payments Received</li>
-                                <li>   Sales Return</li>
-                            </ul>
-                        )}
-
-                        {/* Payments Section */}
-                        <li onClick={() => toggleMenu("payments")}> 
-                            <FaMoneyBillWave className="menu-icon" /> Payments {openMenu.payments ? <MdExpandLess /> : <MdExpandMore />}
-                        </li>
-                        {openMenu.payments && (
-                            <ul className="submenu">
-                                <li>   Vendors</li>
-                                <li>   Purchase Orders</li>
-                                <li>   Purchase Receives</li>
-                                <li>   Bills</li>
-                                <li>   Payments Made</li>
-                            </ul>
-                        )}
-                        
-                        <li><FaChartBar className="menu-icon" /> Reports</li>
-                    </ul>
-                </div>
+                <Sidebar />
 
                 {/* Right Pane */}
                 <div className="right-pane">
@@ -143,41 +86,41 @@ const Home = () => {
                     </div>
 
                     {/* Additional Tables Below */}
-                    <div className="additional-tables-container">
-    {/* Inventory Table */}
-    <div className="inventory-section">
-        <table className="inventory-table">
-            <tr>
-                <th>Category</th>
-                <th>Count</th>
-                <th>Stock Overview</th>  
-            </tr>
-            <tr>
-                <td>Low Stock Items</td>
-                <td><span id="lowStock">0</span></td>
-                <td rowSpan="3" className="chart-cell">
-                    <div className="chart-container">
-                        <div className="hollow-circle">
-                            <span className="percentage">0%</span>
+                  <div className="additional-tables-container">
+                    {/* Inventory Table */}
+                    <div className="inventory-section">
+                        <table className="inventory-table">
+                            <tr>
+                                <th>Category</th>
+                                <th>Count</th>
+                                <th>Stock Overview</th>  
+                            </tr>
+                            <tr>
+                                <td>Low Stock Items</td>
+                                <td><span id="lowStock">0</span></td>
+                                <td rowSpan="3" className="chart-cell">
+                                    <div className="chart-container">
+                                        <div className="hollow-circle">
+                                            <span className="percentage">0%</span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>All Item Groups</td>
+                                <td><span id="itemGroups">0</span></td>
+                            </tr>
+                            <tr>
+                                <td>All Items</td>
+                                <td><span id="allItems">0</span></td>
+                            </tr>
+                            </table>
                         </div>
+
+                                {/* Top Selling Products Section */}
+                                <TopSellingProducts />
+
                     </div>
-                </td>
-            </tr>
-            <tr>
-                <td>All Item Groups</td>
-                <td><span id="itemGroups">0</span></td>
-            </tr>
-            <tr>
-                <td>All Items</td>
-                <td><span id="allItems">0</span></td>
-            </tr>
-        </table>
-    </div>
-
-    {/* Top Selling Products Section */}
-    <TopSellingProducts />
-
-</div>
 
 
 
